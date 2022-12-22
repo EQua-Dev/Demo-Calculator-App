5dac5777bd539fd05c2867e175d2972e1cb2f6d6
Richard Uzor <equality560@gmail.com>
2022-12-22 14:27:23 +0100

    Initial Commit

---
+++ Demo Calculator App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Demo Calculator App
+//
+//  Created by Richard on 22/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
